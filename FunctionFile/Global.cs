5b68e9105886a6122fab943f82dcc65c8fe1fe38
plaskina46 <118421132+plaskina46@users.noreply.github.com>
2022-11-21 11:52:02 +0300

    Add files via upload

---
+++ FunctionFile/Global.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionFile
+{
+    public class Global
+    {
+     public void HelloyWorld(string name)
+        {
+            Console.WriteLine($"привет, {name}") ;
+        }
+        public void sum(int a, int b)
+        {
+            a += b;
+        }
+        public int AgeToSix(int age)
+        {
+            return age +6;
+        }
+        public double d_sqrt(double a)
+        {
+            return Math.Sqrt(Math.Sqrt(a));
+        }
+
+
+    }
+}
